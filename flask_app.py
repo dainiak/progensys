94a1de71213d956f187c1185e9b4e56d05d902be
dainiak <dainiak@gmail.com>
2017-04-13 11:18:39 +0000

    More progress on linear programming generation

--- flask_app.py
+++ flask_app.py
@@ -120,6 +120,9 @@ from collections import defaultdict
 import random
 import flask.ext.login as flask_login
 
+from pulp import LpProblem, LpVariable, LpAffineExpression, LpMinimize, LpConstraint, LpStatus
+import functools, operator
+
 # Security sensitive constants are imported from a file not being synced with github
 from tpbeta_security import *
 
@@ -654,7 +657,7 @@ def preprocess_latex_text():
     return jsonify(result = '')
 
 
-@app.route('/test/view/<int:group_number>/<int:test_number>', methods=['GET','POST'])
+@app.route('/group/<int:group_number>/test/<int:test_number>/view', methods=['GET','POST'])
 @flask_login.login_required
 def view_test(group_number, test_number):
     if flask_login.current_user.username not in teachers:
@@ -898,6 +901,152 @@ def create_test(group_number):
                 'level': topic_levels[int(p.topics)]
             })
             current_variation[p.id] += 1
+
+    return render_template(
+        'test_printout.html',
+        problems = problem_sets,
+        users = users,
+        group = group_number,
+        suggested_test_number = suggested_test_number,
+        date = date.today().isoformat(),
+        max_problems = max_problems,
+        log_info = log_info)
+
+
+@app.route('/test/create/<int:group_number>/lp', methods=['GET','POST'])
+@flask_login.login_required
+def create_test_lp(group_number):
+    max_problems = int(request.args.get('max', 5))
+    current_variation = defaultdict(int)
+
+    if flask_login.current_user.username not in teachers:
+        return "You need to be logged in as a teacher to create tests"
+
+    if request.method == 'POST':
+        return
+
+    users = db.session.query(User)\
+        .join(Learner, Learner.user_id == User.id)\
+        .join(AcademicGroup, Learner.academic_group == AcademicGroup.id)\
+        .filter(AcademicGroup.number == group_number)\
+        .all()
+
+    suggested_test_number = max((x[0] for x in db.session.query(TestLog.test_number).filter(TestLog.user == users[0].id).all()), default = 0) + 1
+
+    users = { u.id : '{0} {1}'.format(u.firstname, u.lastname) for u in users }
+
+    ignored_topic_ids = [x[0] for x in db.session.query(Topic.id).filter(Topic.topic.in_(ignored_topics)).all()]
+
+    event_to_number = { 'SEEN': 0, 'TRIED': 1, 'ALMOST': 2, 'SUCCESS': 3 }
+    trajectory_list = Trajectory.query.first().topics.split('|')
+    trajectory_topics = list(set(trajectory_list))
+    trajectory_list = [i for i in map(int, trajectory_list) if i not in ignored_topic_ids]
+    problems_for_trajectory = Problem.query.filter(Problem.topics.in_(trajectory_topics)).all()
+    problems_for_trajectory = { problem.id: problem for problem in problems_for_trajectory }
+    topic_levels = {t.id: t.level for t in Topic.query.filter(Problem.id.in_(trajectory_topics)).all()}
+    problem_ids = list(problems_for_trajectory.keys())
+
+    clones = defaultdict(set)
+    problems_by_topic = defaultdict(set)
+    for problem_id, problem in problems_for_trajectory.items():
+        problems_by_topic[int(problem.topics)].add(problem_id)
+        if problem.clones and len(problem.clones) > 0:
+            clones[problem_id].update(int(i) for i in problem.clones.split(','))
+            for cid in clones[problem_id]:
+                clones[cid].add(problem_id)
+
+    log_info = ''
+
+    problems_available_for_user = defaultdict(set)
+    user_history = defaultdict(lambda: { i: -1 for i in problem_ids })
+    num_appearances = defaultdict(int)
+    exposed_problems = defaultdict(set)
+    remaining_trajectory_items = dict()
+
+    for user_id in users:
+        counted_problems = set()
+        problems_available_for_user[user_id] = set(problem_ids)
+        user_history_items = History.query.filter(History.user == user_id, History.problem.in_(problem_ids))
+        for h in user_history_items:
+            if h.event in event_to_number:
+                user_history[user_id][h.problem] = max(user_history[user_id][h.problem], event_to_number[h.event])
+                num_appearances[h.problem] += 1
+                exposed_problems[user_id].add(h.problem)
+        user_remaining_trajectory_items = trajectory_list[:]
+        for i, t in enumerate(user_remaining_trajectory_items):
+            for problem_id in problems_by_topic[t]:
+                if user_history[user_id][problem_id] >= 2 and problem_id not in counted_problems:
+                    user_remaining_trajectory_items[i] = 0
+                    problems_available_for_user[user_id].remove(problem_id)
+                    if problem_id in clones:
+                        problems_available_for_user[user_id] -= clones[problem_id]
+                    break
+        problems_for_user_uncovered_topics = set()
+        user_remaining_trajectory_items = list(filter(None, user_remaining_trajectory_items))
+        for topic_id in user_remaining_trajectory_items:
+            problems_for_user_uncovered_topics.update(problems_by_topic[topic_id])
+        problems_available_for_user[user_id].intersection_update(problems_for_user_uncovered_topics)
+        remaining_trajectory_items[user_id] = user_remaining_trajectory_items
+
+    penalty_for_having_same_problem_on_next_quiz = 1
+    penalty_for_not_giving_a_brand_new_problem = 1
+    penalty_for_giving_clone_of_exposed_problem = 1
+    penalty_for_having_underflow = 1000
+
+    lp = LpProblem(name = 'Test Generation', sense = LpMinimize)
+    lp_objective = LpAffineExpression()
+
+    lp_variables = dict()
+    for user_id in users:
+        for problem_id in problems_available_for_user[user_id]:
+            lp_variables[(user_id, problem_id)] = LpVariable(name = 'assign_{}_{}'.format(user_id, problem_id), cat='Binary')
+
+    for user_id in users:
+        # Number of problems for user should definitely not exceed the maximum
+        lp += (sum(lp_variables[(user_id, problem_id)] for problem_id in problems_available_for_user[user_id]) <= max_problems)
+        # Number of problems for user should preferably not be less than minimum
+        min_problems = min(max_problems, len(problems_available_for_user[user_id]), len(remaining_trajectory_items))
+        lp_underflow_variable = LpVariable('underflow_{}'.format(user_id), lowBound = 0)
+        lp += (sum(lp_variables[(user_id, problem_id)] for problem_id in problems_available_for_user[user_id]) + lp_underflow_variable >= min_problems)
+        lp_objective += penalty_for_having_underflow * lp_underflow_variable
+
+    for user_id in users:
+        for topic_id in remaining_trajectory_items[user_id]:
+            problems_for_user_and_topic = problems_by_topic[topic_id] & problems_available_for_user[user_id]
+            if len(problems_for_user_and_topic) >= 2:
+                # The number of problems for any topic should not exceed the number of times this topic appears in a trajectory
+                # TODO: make it work when the number of times exceeds 2
+                lp += (sum(lp_variables[(user_id, problem_id)] for problem_id in problems_for_user_and_topic ) <= 1)
+
+    for user_id in users:
+        for problem_id in problems_available_for_user[user_id]:
+            if problem_id in exposed_problems[user_id]:
+                lp_objective += penalty_for_having_same_problem_on_next_quiz * lp_variables[(user_id, problem_id)]
+            if num_appearances[problem_id] > 0:
+                lp_objective += penalty_for_not_giving_a_brand_new_problem * lp_variables[(user_id, problem_id)]
+        clones_of_exposed_problems = functools.reduce(operator.or_, (clones[problem_id] for problem_id in exposed_problems[user_id]), set()) - exposed_problems[user_id]
+        for problem_id in problems_available_for_user[user_id] & clones_of_exposed_problems:
+            lp_objective += penalty_for_giving_clone_of_exposed_problem * lp_variables[(user_id, problem_id)]
+
+    lp.setObjective(lp_objective)
+
+    try:
+        lp.solve()
+        log_info = 'Problem status is “{}” with objective function value “{}”'.format(LpStatus[lp.status], lp.objective.value())
+    except:
+        log_info = 'Failed to solve the linear program'
+
+    problem_sets = defaultdict(list)
+    for user_id, problem_id in lp_variables:
+        if lp_variables[(user_id, problem_id)].value() > 0:
+            problem = problems_for_trajectory[problem_id]
+            problem_sets[user_id].append({
+                'id': problem_id,
+                'text': latex_to_html(problem.statement, variation=current_variation[problem_id]),
+                'level': topic_levels[int(problem.topics)]
+            })
+
+            current_variation[problem_id] += 1
 
     return render_template(
         'test_printout.html',
