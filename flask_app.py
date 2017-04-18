72c1a47e3118d0ff98e11c057c981d0d031fbb70
Alex Dainiak <dainiak@gmail.com>
2017-04-18 16:03:10 +0300

    PEP8 refactoring

--- flask_app.py
+++ flask_app.py
@@ -17,7 +17,7 @@ from functools import reduce
 from hashlib import md5 as md5hasher
 
 from json import dumps as to_json_string
-#from json import loads as from_json_string
+# from json import loads as from_json_string
 
 
 # Security sensitive constants are imported from a file not being synced with github
@@ -321,6 +321,7 @@ class TrajectoryContent(db.Model):
         if sort_key is not None:
             self.sort_key = sort_key
 
+
 class ProblemStatus(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     problem_id = db.Column(db.Integer, db.ForeignKey('problem.id'), primary_key=True)
@@ -574,23 +575,60 @@ def api_exposure():
 
         exposure_ids = json.get('exposure_ids')
         user_ids = json.get('user_ids')
-        db_data = db.session.query(User.name_last, User.name_first, User.id, Exposure.timestamp, Exposure.id, ExposureContent.problem_set_id).filter(User.id.in_(user_ids), Exposure.id.in_(exposure_ids), ExposureContent.user_id == User.id, ExposureContent.exposure_id == Exposure.id).all()
+        db_data = db.session.query(
+            User.name_last,
+            User.name_first,
+            User.id,
+            Exposure.timestamp,
+            Exposure.id,
+            ExposureContent.problem_set_id
+        ).filter(
+            User.id.in_(user_ids),
+            Exposure.id.in_(exposure_ids),
+            ExposureContent.user_id == User.id,
+            ExposureContent.exposure_id == Exposure.id
+        ).all()
         data_for_frontend = []
 
-        for user_name_last, user_name_first, user_id, exposure_timestamp, exposure_id, problem_set_id in db_data:
+        for user_name_last, \
+            user_name_first, \
+            user_id, \
+            exposure_timestamp, \
+            exposure_id, \
+            problem_set_id \
+                in db_data:
             data_for_frontend.append(dict())
             data_for_frontend[-1]['user_id'] = user_id
             data_for_frontend[-1]['exposure_id'] = exposure_id
             data_for_frontend[-1]['problem_set_id'] = problem_set_id
             data_for_frontend[-1]['exposure_date'] = exposure_timestamp.isoformat()[:10]
             data_for_frontend[-1]['learner_name'] = '{} {}'.format(user_name_last, user_name_first)
-            problem_ids = list(map(lambda x: x[0], db.session.query(ProblemSetContent.problem_id).filter(ProblemSetContent.problem_set_id == problem_set_id).order_by(ProblemSetContent.sort_key).all()))
-            graded_problems = db.session.query(ExposureGradingResult.problem_id, ExposureGradingResult.problem_status_id).filter(ExposureGrading.exposure_id == exposure_id, ExposureGradingResult.exposure_grading_id == ExposureGrading.id, ExposureGradingResult.user_id == user_id, ExposureGradingResult.problem_set_id == problem_set_id).all()
+            problem_ids = list(map(
+                lambda x: x[0],
+                db.session.query(
+                    ProblemSetContent.problem_id
+                ).filter(
+                    ProblemSetContent.problem_set_id == problem_set_id
+                ).order_by(
+                    ProblemSetContent.sort_key
+                ).all()))
+            graded_problems = db.session.query(
+                ExposureGradingResult.problem_id,
+                ExposureGradingResult.problem_status_id
+            ).filter(
+                ExposureGrading.exposure_id == exposure_id,
+                ExposureGradingResult.exposure_grading_id == ExposureGrading.id,
+                ExposureGradingResult.user_id == user_id,
+                ExposureGradingResult.problem_set_id == problem_set_id
+            ).all()
             graded_problems = {p[0]: p[1] for p in graded_problems}
 
             for problem_position, problem_id in enumerate(problem_ids):
                 problem_position += 1
-                data_for_frontend[-1]['p{}'.format(problem_position)] = graded_problems.get(problem_id, -problem_id)
+                data_for_frontend[-1]['p{}'.format(problem_position)] = graded_problems.get(
+                    problem_id,
+                    -problem_id
+                )
                 data_for_frontend[-1]['pid{}'.format(problem_position)] = problem_id
 
         return jsonify(data_for_frontend)
@@ -604,14 +642,24 @@ def api_exposure():
         problem_set_id = item.get('problem_set_id')
         if None in [user_id, exposure_id, problem_set_id]:
             return jsonify(error='Data required for deleting exposure record was not provided')
-        exposure_record = ExposureContent.query.filter_by(exposure_id=exposure_id, user_id=user_id, problem_set_id=problem_set_id).first()
+        exposure_record = ExposureContent.query.filter_by(
+            exposure_id=exposure_id,
+            user_id=user_id,
+            problem_set_id=problem_set_id
+        ).first()
         if not exposure_record:
             return jsonify(error='The specified exposure record was not found')
         db.session.delete(exposure_record)
         db.session.commit()
         result_report = 'Deleted the exposure record.'
         problem_set = ProblemSet.query.filter_by(id=problem_set_id).first()
-        if (problem_set.is_adhoc and not db.session.query(ExposureContent.id).filter(ExposureContent.problem_set_id == problem_set_id).first()):
+        if (problem_set.is_adhoc
+            and not db.session.query(
+                ExposureContent.id
+            ).filter(
+                ExposureContent.problem_set_id == problem_set_id
+            ).first()
+                ):
             db.session.delete(problem_set)
             db.session.commit()
             result_report += ' The problem set was ad hoc and not used elsewhere, so deleted it too.'
@@ -648,7 +696,13 @@ def api_exposure():
                     if problem_id in existing_grading_results:
                         existing_grading_results[problem_id].problem_status_id = problem_status_id
                     else:
-                        grading_result = ExposureGradingResult(grading.id, user_id, problem_set_id, problem_id, problem_status_id)
+                        grading_result = ExposureGradingResult(
+                            grading.id,
+                            user_id,
+                            problem_set_id,
+                            problem_id,
+                            problem_status_id
+                        )
                         db.session.add(grading_result)
                 else:
                     item['p{}'.format(problem_no)] = -problem_id
@@ -664,21 +718,34 @@ def view_exposure_table(exposure_date):
         return "Login required for this URL"
 
     exposures = Exposure.query.all()
-    exposure_ids = list(map(attrgetter('id'), filter(lambda x: x.timestamp.isoformat()[:10] == exposure_date, exposures)))
+    exposure_ids = list(map(
+        attrgetter('id'),
+        filter(lambda x: x.timestamp.isoformat()[:10] == exposure_date, exposures)
+    ))
     if len(exposure_ids) == 0:
         return jsonify(error='No exposures with specified date were found')
 
     user_ids = list(map(lambda x: x[0], db.session.query(User.id).all()))
 
-    all_problems = list(map(lambda x: x[0], db.session.query(Problem.id)
-        .join(ProblemSetContent, Problem.id == ProblemSetContent.problem_id)
-        .join(ExposureContent, ExposureContent.problem_set_id == ProblemSetContent.problem_set_id)
-        .filter(ExposureContent.exposure_id.in_(exposure_ids))
-        .filter(ExposureContent.user_id.in_(user_ids))
-        .all()))
+    all_problems = list(map(
+        lambda x: x[0],
+        db.session.query(
+            Problem.id
+        ).filter(
+            ExposureContent.problem_set_id == ProblemSetContent.problem_set_id,
+            Problem.id == ProblemSetContent.problem_id,
+            ExposureContent.exposure_id.in_(exposure_ids),
+            ExposureContent.user_id.in_(user_ids)
+        ).all()))
     problem_statuses = [{'name': ps.icon, 'id': ps.id} for ps in ProblemStatusInfo.query.all()]
 
-    return render_template('view_exposures.html', all_problems=to_json_string(all_problems), exposure_ids=to_json_string(exposure_ids), user_ids=to_json_string(user_ids), problem_statuses=to_json_string(problem_statuses))
+    return render_template(
+        'view_exposures.html',
+        all_problems=to_json_string(all_problems),
+        exposure_ids=to_json_string(exposure_ids),
+        user_ids=to_json_string(user_ids),
+        problem_statuses=to_json_string(problem_statuses)
+    )
 
 
 # ------------------------------------------
@@ -702,23 +769,60 @@ def api_grading():
 
         exposure_ids = json.get('exposure_ids')
         user_ids = json.get('user_ids')
-        db_data = db.session.query(User.name_last, User.name_first, User.id, Exposure.timestamp, Exposure.id, ExposureContent.problem_set_id).filter(User.id.in_(user_ids), Exposure.id.in_(exposure_ids), ExposureContent.user_id == User.id, ExposureContent.exposure_id == Exposure.id).all()
+        db_data = db.session.query(
+            User.name_last,
+            User.name_first,
+            User.id,
+            Exposure.timestamp,
+            Exposure.id,
+            ExposureContent.problem_set_id
+        ).filter(
+            User.id.in_(user_ids),
+            Exposure.id.in_(exposure_ids),
+            ExposureContent.user_id == User.id,
+            ExposureContent.exposure_id == Exposure.id
+        ).all()
         data_for_frontend = []
 
-        for user_name_last, user_name_first, user_id, exposure_timestamp, exposure_id, problem_set_id in db_data:
+        for user_name_last, \
+            user_name_first, \
+            user_id, \
+            exposure_timestamp, \
+            exposure_id, \
+            problem_set_id \
+                in db_data:
             data_for_frontend.append(dict())
             data_for_frontend[-1]['user_id'] = user_id
             data_for_frontend[-1]['exposure_id'] = exposure_id
             data_for_frontend[-1]['problem_set_id'] = problem_set_id
             data_for_frontend[-1]['exposure_date'] = exposure_timestamp.isoformat()[:10]
             data_for_frontend[-1]['learner_name'] = '{} {}'.format(user_name_last, user_name_first)
-            problem_ids = list(map(lambda x: x[0], db.session.query(ProblemSetContent.problem_id).filter(ProblemSetContent.problem_set_id == problem_set_id).order_by(ProblemSetContent.sort_key).all()))
-            graded_problems = db.session.query(ExposureGradingResult.problem_id, ExposureGradingResult.problem_status_id).filter(ExposureGrading.exposure_id == exposure_id, ExposureGradingResult.exposure_grading_id == ExposureGrading.id, ExposureGradingResult.user_id == user_id, ExposureGradingResult.problem_set_id == problem_set_id).all()
+            problem_ids = list(map(
+                lambda x: x[0],
+                db.session.query(
+                    ProblemSetContent.problem_id
+                ).filter(
+                    ProblemSetContent.problem_set_id == problem_set_id
+                ).order_by(
+                    ProblemSetContent.sort_key
+                ).all()))
+            graded_problems = db.session.query(
+                ExposureGradingResult.problem_id,
+                ExposureGradingResult.problem_status_id
+            ).filter(
+                ExposureGrading.exposure_id == exposure_id,
+                ExposureGradingResult.exposure_grading_id == ExposureGrading.id,
+                ExposureGradingResult.user_id == user_id,
+                ExposureGradingResult.problem_set_id == problem_set_id
+            ).all()
             graded_problems = {p[0]: p[1] for p in graded_problems}
 
             for problem_position, problem_id in enumerate(problem_ids):
                 problem_position += 1
-                data_for_frontend[-1]['p{}'.format(problem_position)] = graded_problems.get(problem_id, -problem_id)
+                data_for_frontend[-1]['p{}'.format(problem_position)] = graded_problems.get(
+                    problem_id,
+                    -problem_id
+                )
                 data_for_frontend[-1]['pid{}'.format(problem_position)] = problem_id
 
         return jsonify(data_for_frontend)
@@ -754,7 +858,13 @@ def api_grading():
                     if problem_id in existing_grading_results:
                         existing_grading_results[problem_id].problem_status_id = problem_status_id
                     else:
-                        grading_result = ExposureGradingResult(grading.id, user_id, problem_set_id, problem_id, problem_status_id)
+                        grading_result = ExposureGradingResult(
+                            grading.id,
+                            user_id,
+                            problem_set_id,
+                            problem_id,
+                            problem_status_id
+                        )
                         db.session.add(grading_result)
                 else:
                     item['p{}'.format(problem_no)] = -problem_id
@@ -774,16 +884,19 @@ def view_grading_table(exposure_date, group=None):
         return jsonify(error='Group not found')
 
     exposures = Exposure.query.all()
-    exposure_ids = list(map(attrgetter('id'), filter(lambda x: x.timestamp.isoformat()[:10] == exposure_date, exposures)))
+    exposure_ids = list(map(
+        attrgetter('id'),
+        filter(lambda x: x.timestamp.isoformat()[:10] == exposure_date, exposures)
+    ))
     if len(exposure_ids) == 0:
         return jsonify(error='No exposures with specified date were found')
 
     if group is not None:
         group_id = Group.query.filter_by(code=group).first().id
 
-        user_ids = list(map(lambda x: x[0],
-            db.session.query(User.id)
-            .filter(
+        user_ids = list(map(
+            lambda x: x[0],
+            db.session.query(User.id).filter(
                 GroupMembership.group_id == group_id,
                 GroupMembership.user_id == User.id,
                 ExposureContent.exposure_id.in_(exposure_ids),
@@ -792,8 +905,11 @@ def view_grading_table(exposure_date, group=None):
     else:
         user_ids = list(map(lambda x: x[0], db.session.query(User.id).all()))
 
-    all_problems = list(map(lambda x: x[0], db.session.query(Problem.id)
-        .filter(
+    all_problems = list(map(
+        lambda x: x[0],
+        db.session.query(
+            Problem.id
+        ).filter(
             Problem.id == ProblemSetContent.problem_id,
             ExposureContent.problem_set_id == ProblemSetContent.problem_set_id,
             ExposureContent.exposure_id.in_(exposure_ids),
@@ -801,7 +917,13 @@ def view_grading_table(exposure_date, group=None):
         .all()))
     problem_statuses = [{'name': ps.icon, 'id': ps.id} for ps in ProblemStatusInfo.query.all()]
 
-    return render_template('view_grading_results.html', all_problems=to_json_string(all_problems), exposure_ids=to_json_string(exposure_ids), user_ids=to_json_string(user_ids), problem_statuses=to_json_string(problem_statuses))
+    return render_template(
+        'view_grading_results.html',
+        all_problems=to_json_string(all_problems),
+        exposure_ids=to_json_string(exposure_ids),
+        user_ids=to_json_string(user_ids),
+        problem_statuses=to_json_string(problem_statuses)
+    )
 
 
 # ------------------------------------------
@@ -823,7 +945,15 @@ def api_trajectory():
 
         trajectory_id = json['trajectory_id']
 
-        data = sorted(db.session.query(TrajectoryContent.sort_key, TrajectoryContent.id, TrajectoryContent.topic_id, Topic.code).filter(TrajectoryContent.trajectory_id == trajectory_id, Topic.id == TrajectoryContent.topic_id).all())
+        data = sorted(db.session.query(
+            TrajectoryContent.sort_key,
+            TrajectoryContent.id,
+            TrajectoryContent.topic_id,
+            Topic.code
+        ).filter(
+            TrajectoryContent.trajectory_id == trajectory_id,
+            Topic.id == TrajectoryContent.topic_id
+        ).all())
         result = []
         for sort_key, id, topic_id, topic_code in data:
             result.append({
@@ -841,7 +971,12 @@ def api_trajectory():
 
         trajectory_id = json['trajectory_id']
         new_order = json['ids']
-        items = {tc.id: tc for tc in db.session.query(TrajectoryContent).filter(TrajectoryContent.trajectory_id == trajectory_id, TrajectoryContent.id.in_(new_order)).all()}
+        items = {tc.id: tc for tc in db.session.query(
+            TrajectoryContent
+        ).filter(
+            TrajectoryContent.trajectory_id == trajectory_id,
+            TrajectoryContent.id.in_(new_order)
+        ).all()}
 
         for i, item_id in enumerate(new_order):
             items[item_id].sort_key = 10 * (i + 1)
